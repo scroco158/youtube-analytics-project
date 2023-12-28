368711902febc8cd314942a10f84b90eca6b3eb5
scroco158 <scroco@mail.ru>
2023-12-28 21:56:48 +0300

    DZ 4

--- src/video.py
+++ src/video.py
@@ -1,21 +1,23 @@
 import os
 from googleapiclient.discovery import build
 import json
-
+from src.channel import Channel
 
 class Video:
     api_key: str = os.getenv('YT_API_KEY')
     youtube = build("youtube", "v3", developerKey=api_key)
 
     def __init__(self, id_video):
-        self.id_video = id_video
-        self.request = self.youtube.videos().list(part="snippet,contentDetails", id=self.id_video)
+        self.id_video = id_video  # id видео
+        self.request = self.youtube.videos().list(part="snippet,statistics", id=self.id_video)
         self.response = self.request.execute()
-        #print(self.response)
-        self.name = self.response['items'][0]['snippet']['title']
-        # print(self.name)
-        # print(self.response)
+        self.name = self.response['items'][0]['snippet']['title']  # название видео
+        self.view_count = self.response['items'][0]['statistics']['viewCount']  # просмотры
+        self.like_count = self.response['items'][0]['statistics']['likeCount']  # лайки
+        self.url = f'https://www.youtube.com/watch?v={self.id_video}'
 
+        #print(self.url)
+        # print(self.response)
         # with open('video_data.json', 'w') as f:
         #     json.dump(self.response, f, indent=4, ensure_ascii=False)
 
@@ -23,5 +25,8 @@ class Video:
         return self.name
 
 
-class PLVideo:
-    pass
+class PLVideo(Video):
+
+    def __init__(self, vid_id, pl_id):
+        super().__init__(vid_id)
+        self.pl_id = pl_id
